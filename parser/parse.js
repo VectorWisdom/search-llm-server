768d72a7c5614db443e75016d6886b7ab0099ad4
wass <wassim.filali@gmail.com>
2023-12-19 12:11:56 +0100

    started wit hparser

---
+++ parser/parse.js
@@ -0,0 +1,12 @@
+import {collect} from 'content-structure'
+import {fileURLToPath} from 'url';
+import {dirname} from 'path'
+
+await collect({
+    rootdir:dirname(fileURLToPath(import.meta.url)),
+    rel_contentdir:"../markdown",
+    content_ext:["md","json","yml","yaml"],
+    assets_ext:["svg","webp","png","jpeg","jpg","xlsx","glb"],
+    rel_outdir:"../.structure",
+    debug:true
+})
